f0e888aa988791223731351f961fe24c957f291e
amcolling <amcolling@gmail.com>
2018-05-09 09:07:37 -0600

    initial commit

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -1,1 +1,20 @@
-# YOUR TESTS GO IN HERE!
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/translator'
+
+
+class TranslatorTest < Minitest::Test
+  def test_it_exists
+  translator = Translator.new
+  assert_instance_of Translator, translator
+  end
+end 
+
+
+# Translate English to Morse Code
+#
+# lowercase letters
+#   $ translator = Translate.new
+#   => #<Translate:0x007fa1ab98cac0>
+#   $ translator.eng_to_morse("hello world")
+#   => "......-...-..--- .-----.-..-..-.."
